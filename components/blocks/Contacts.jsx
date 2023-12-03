f9712f054a03ef028e7f31bc78c9491493b8a9b4
Егор Ткаченко <egortkachenco@gmail.com>
2023-12-03 20:00:02 +0200

    header and contacts update

--- components/blocks/Contacts.jsx
+++ components/blocks/Contacts.jsx
@@ -5,7 +5,7 @@ const Contacts = () => {
   return (
     <footer
       id="contacts"
-      className="bg-gray-800 text-gray-200 lg:p-20 py-10 px-8 flex flex-col lg:flex-row gap-6"
+      className="bg-gray-800 text-gray-200 lg:p-20 py-10 px-8 flex flex-col xl:flex-row gap-6"
     >
       <div className="flex-grow">
         <h3 className="uppercase text-2xl mb-3" data-aos="fade-right">
@@ -30,30 +30,24 @@ const Contacts = () => {
             title="Наші контакти:"
             text={
               <div>
-                <a
-                  href={constants.phone_1.link}
-                  className="text-red-300 hover:underline"
-                >
+                <a href={constants.phone_1.link} className="hover:underline">
                   {constants.phone_1.label}
                 </a>
-                <span className="text-gray-400">{' ' + '|' + ' '}</span>
-                <a
-                  href={constants.phone_2.link}
-                  className="text-red-300 hover:underline"
-                >
+                <span className="text-white hidden lg:inline">
+                  {' ' + '|' + ' '}
+                </span>
+                <br className="block lg:hidden" />
+                <a href={constants.phone_2.link} className="hover:underline">
                   {constants.phone_2.label}
                 </a>
               </div>
             }
           />
           <ContactItem
-            icon="/icons/mail.svg"
+            icon="/icons/email.svg"
             title="Електрона пошта:"
             text={
-              <a
-                href={constants.email.link}
-                className="text-red-300 hover:underline"
-              >
+              <a href={constants.email.link} className="hover:underline">
                 {constants.email.label}
               </a>
             }
@@ -61,27 +55,28 @@ const Contacts = () => {
           <ContactItem
             icon="/icons/location.svg"
             title="Наша адреса:"
-            text={
-              <address className="text-red-300">{constants.address}</address>
-            }
+            text={<address>{constants.address}</address>}
           />
         </div>
       </div>
 
       <div
-        className="lg:w-1/3 relative xl:h-96 xl:ms-auto h-64 w-96 mx-auto"
+        className="xl:w-1/3 relative xl:ms-auto w-96 mx-auto"
         data-aos="fade-left"
       >
-        <a href="https://maps.app.goo.gl/ncG4dBs51e4uAz627" target="_blank">
-          <Image
-            src="/icons/map-marker.svg"
-            alt="marker"
-            width={28}
-            height={40}
-            className="absolute z-10 top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 hover:scale-125 transition-all"
-          />
-        </a>
-        <Image src="/map.png" alt="map" fill objectFit="fill" />
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.700317967283!2d30.407473977239825!3d50.44668247159156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cc6b85d9062d%3A0x2048d4b8ea57f795!2z0JDQtNCy0LXQvdGC0LAg0J_RgNC-0YLQuNCy0L7Qv9C-0LbQsNGA0L3Ri9C1INCU0LLQtdGA0Lg!5e0!3m2!1suk!2sua!4v1701618240838!5m2!1suk!2sua"
+          style={{
+            border: 0,
+            width: '100%',
+            height: '100%',
+            maxWidth: '500px',
+            minHeight: '350px',
+          }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
       </div>
     </footer>
   );
@@ -90,11 +85,11 @@ const Contacts = () => {
 export default Contacts;
 
 const ContactItem = ({ icon, title, text }) => (
-  <div className="flex gap-8 items-center">
-    <div className="flex items-center gap-2 w-44">
+  <div className="flex gap-8 lg:items-center items-start">
+    <div className="flex items-center gap-2 lg:w-44 w-2/4">
       <Image src={icon} alt="icon" width={24} height={16} />
       {title}
     </div>
-    {text}
+    <div className="w-2/4">{text}</div>
   </div>
 );
--- components/layout/Navigation.jsx
+++ components/layout/Navigation.jsx
@@ -2,9 +2,10 @@ import routes from '@/routes';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import constants from '@/constants';
-import LinkTransition from '../LinkTransition';
+import { useLottie } from 'lottie-react';
+import menuAnimation from './Animation - 1701624342853.json';
 
 const links = [
   { label: 'ГОЛОВНА', link: '/' },
@@ -14,70 +15,100 @@ const links = [
   { label: 'ГРАТИ', link: '#grilles' },
   { label: 'КОНТАКТИ', link: '#contacts' },
 ];
+
 const Navigation = () => {
-  const router = useRouter();
+  const animation = useLottie({
+    animationData: menuAnimation,
+    loop: false,
+    autoplay: false,
+  });
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!open);
+    if (!open) {
+      animation.playSegments([0, 80], true);
+    } else {
+      animation.playSegments([80, 160], true);
+    }
+  };
 
   return (
-    <nav className="bg-white text-black border-b-2 border-gray-600">
-      <div className="flex gap-12 items-center justify-end py-3 px-20 text-sm">
-        <div className="flex items-center gap-1">
-          <Image
-            src="/icons/calendar.svg"
-            alt="calendar"
-            width={16}
-            height={16}
-          />
-          {constants.work_hours}
-        </div>
-
-        <div className="flex items-center gap-1">
-          <Image
-            src="/icons/location.svg"
-            alt="calendar"
-            width={16}
-            height={16}
-          />
-          {constants.address}
-        </div>
-
-        <div className="flex items-center gap-1">
-          <Image src="/icons/mail.svg" alt="mail" width={16} height={16} />
-          {constants.email.label}
-        </div>
-
-        <div className="flex items-center gap-1">
-          <Image src="/icons/phone.svg" alt="phone" width={16} height={16} />
-          <a href={constants.phone_1.link} className="hover:underline">
-            {constants.phone_1.label}
-          </a>
-          {' ' + '|' + ' '}
-          <a href={constants.phone_2.link} className="hover:underline">
-            {constants.phone_2.label}
-          </a>
-        </div>
+    <nav className="bg-white text-black border-b-2 border-gray-600 relative">
+      <div className="gap-12 items-center justify-end py-3 xl:px-20 px-10 text-sm hidden xl:flex">
+        <ContactsList />
       </div>
 
-      <div className="flex gap-12 items-center justify-end py-3 px-20">
+      <div className="flex gap-12 items-center py-3 xl:px-20 px-10 bg-white relative z-20">
         <div className="flex items-center">
           <Image src="/nav-logo.svg" alt="logo" height={32} width={138} />
         </div>
 
-        <div className="ml-auto mr-8 flex gap-12">
-          {links.map((link, i) => {
-            return (
-              <Link
-                key={i}
-                href={link.link}
-                className="flex items-center text-gray-600  hover:text-black  transition-all font-medium border-b-2 border-transparent hover:border-black"
-              >
-                {link.label}
-              </Link>
-            );
-          })}
+        <div className="ml-auto mr-8 gap-12 hidden xl:flex">
+          <LinksList links={links} />
         </div>
+
+        <button
+          className="w-12 h-12 ml-auto xl:hidden flex"
+          onClick={toggleMenu}
+        >
+          {animation.View}
+        </button>
+      </div>
+
+      <div
+        className={`transition-all duration-1000 absolute w-full bg-white z-10 p-10 left-0 border-y-2 border-gray-600 space-y-4 ${
+          open ? 'top-full' : '-top-full -translate-y-full'
+        }`}
+      >
+        <LinksList />
+        <hr />
+        <ContactsList />
       </div>
     </nav>
   );
 };
 
 export default Navigation;
+
+const ContactsList = () => (
+  <>
+    <div className="flex items-center gap-1">
+      <Image src="/icons/calendar.svg" alt="calendar" width={16} height={16} />
+      {constants.work_hours}
+    </div>
+    <div className="flex items-center gap-1">
+      <Image src="/icons/location.svg" alt="calendar" width={16} height={16} />
+      {constants.address}
+    </div>
+    <div className="flex items-center gap-1">
+      <Image src="/icons/email.svg" alt="mail" width={16} height={16} />
+      <a href={constants.email.link} className="hover:underline">
+        {constants.email.label}
+      </a>
+    </div>
+    <div className="flex items-center gap-1">
+      <Image src="/icons/phone.svg" alt="phone" width={16} height={16} />
+      <a href={constants.phone_1.link} className="hover:underline">
+        {constants.phone_1.label}
+      </a>
+      {' ' + '|' + ' '}
+      <a href={constants.phone_2.link} className="hover:underline">
+        {constants.phone_2.label}
+      </a>
+    </div>
+  </>
+);
+
+const LinksList = () =>
+  links.map((link, i) => {
+    return (
+      <Link
+        key={i}
+        href={link.link}
+        className="flex items-center text-gray-600  hover:text-black  transition-all font-medium border-b-2 border-transparent hover:border-black"
+      >
+        {link.label}
+      </Link>
+    );
+  });
